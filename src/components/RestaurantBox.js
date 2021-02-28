c75c042fcbf59ac1bac93fb77c1f1eeccf26b53e
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-28 18:38:33 -0500

    RestaurantBox form setup

--- src/components/RestaurantBox.js
+++ src/components/RestaurantBox.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class RestaurantBox extends React.Component {
+  state = { term: '' };
+
+  handleChange = (e) => {
+    this.setState({ term: e.target.value });
+  }
+
+  
   render(){
+    console.log(this.state.term);
     return (
       <div>
-        RestaurantBox Component
+        <form>
+          <h4>Search for your restaurant</h4>
+          <input type="text" value={this.state.term} onChange={this.handleChange}/>
+          <button>Search</button>
+        </form>
       </div>
     )
   }
